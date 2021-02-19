dd2dc64e3313c78b8a6b38c69997552bee4a08df
chaticker <cseun999@naver.com>
2021-02-19 13:48:43 +0900

    38. commit

--- src/router/index.js
+++ src/router/index.js
@@ -47,9 +47,9 @@ const routes = [
     component: () => import('../views/renderer')
   },
   {
-    path: '*',
-    name: 'error',
-    component: () => import('../views/error')
+    path: '/:collection/:document/:action',
+    name: 'collection-document-action',
+    component: () => import('../views/renderer')
   }
 ]
 
